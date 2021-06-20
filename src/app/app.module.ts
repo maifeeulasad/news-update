e13199efd197abdbf3d4d12b6ebd02f92b8633b0
maifeeulasad <maifeeulasad@gmail.com>
2021-06-20 17:43:29 +0600

    //wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ElementComponent } from './component/news/element/element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ElementComponent
   ],
   imports: [
     BrowserModule
