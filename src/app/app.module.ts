2a8d92ed7071a9624222ec8f6e61652806da7f35
maifeeulasad <maifeeulasad@gmail.com>
2021-06-20 17:34:11 +0600

    created empty component news element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ElementComponent } from './component/news/element/element.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ElementComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/component/news/element/element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-element',
+  templateUrl: './element.component.html',
+  styleUrls: ['./element.component.css']
+})
+export class ElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
