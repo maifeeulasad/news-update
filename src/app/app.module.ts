c5349935b2d47400aba56d14e806bd5c00fb4c54
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-03-21 20:30:51 +0600

    //wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
+import { MatLinkPreviewModule } from '@angular-material-extensions/link-preview';
 
 import { AppComponent } from './app.component';
 import { NewsElementComponent } from './component/news-element/news-element.component';
@@ -13,6 +14,7 @@ import { NewsElementComponent } from './component/news-element/news-element.comp
   imports: [
     BrowserModule,
     HttpClientModule,
+    MatLinkPreviewModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
