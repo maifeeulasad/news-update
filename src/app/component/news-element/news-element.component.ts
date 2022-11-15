07581c0d635146bd5224963045da57255892aaef
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-11-15 19:19:27 +0600

    [feature]: call proxy server for og-details; ref: `https://github.com/maifeeulasad/proxy-server`

--- src/app/component/news-element/news-element.component.ts
+++ src/app/component/news-element/news-element.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { INewsElement } from '../../model/INewsElement';
 import * as htmlMetadataParser from 'html-metadata-parser';
 
+interface IOg {
+  description?: string;
+  image?: string;
+}
+
 @Component({
   selector: 'app-news-element',
   templateUrl: './news-element.component.html',
@@ -18,17 +23,40 @@ export class NewsElementComponent implements OnInit {
     };
   }
 
+  ogToNewsElement({ description, image }: IOg) {
+    this.newsElement.description = description;
+    this.newsElement.image = image;
+  }
+
+  fetchOg(): Promise<IOg> {
+    return new Promise((resolve) => {
+      htmlMetadataParser
+        .parse(this.newsElement.url)
+        .then(({ og }: { og: IOg }) => {
+          resolve(og);
+        })
+        .catch(() => {
+          htmlMetadataParser
+            .parse('https://proxy-server-mua.herokuapp.com/proxy', {
+              method: 'POST',
+              data: {
+                url: this.newsElement.url,
+              },
+            })
+            .then(({ og }: { og: IOg }) => {
+              resolve(og);
+            })
+            .catch(() => {
+              resolve({ image: './assets/news.jpg' });
+            });
+        });
+    });
+  }
+
   ngOnInit(): void {
     this.newsElement.image = './assets/loading.gif';
-    htmlMetadataParser
-      .parse(this.newsElement.url)
-      .then(({ og }) => {
-        const { description, image } = og;
-        this.newsElement.description = description;
-        this.newsElement.image = image;
-      })
-      .catch(() => {
-        this.newsElement.image = './assets/news.jpg';
-      });
+    this.fetchOg().then((og) => {
+      this.ogToNewsElement(og);
+    });
   }
 }
